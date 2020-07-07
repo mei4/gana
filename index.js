01ad7ce1d295978620539a8b70e1b04bb2669595
Clara Gaset <claragaset4@gmail.com>
2020-07-07 16:11:36 +0200

    Replaces openapi json for yaml

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const routes = require('./src/routes')
 const swaggerUi = require('swagger-ui-express');
-const openApiDocumentation = require('./openApiDocumentation');
+const YAML = require('yamljs');
+const openApiDocument = YAML.load('./openapi.yaml');
 
 const hostname = "127.0.0.1";
 const port = 4000;
@@ -12,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 app.use((req, res, next) => {
    res.header("Access-Control-Allow-Origin", "*")
    next()
