7f9bd8b97722930acd3ff289992aec8a1da18733
Clara Gaset <claragaset4@gmail.com>
2020-07-10 10:27:36 +0200

    Adds test for GET by id

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -16,31 +16,58 @@ afterAll(async () => await dbHandler.closeDatabase());
 describe('when a GET method is called', () => {
 
 	describe('when the route is /', () => {
+		
+		describe('when status code is 200', () => {	
+			test("it should return a HTML", () => {
+				return request(app)
+				.get("/")
+				.then(response => {
+					expect(response.statusCode).toBe(200);
+					expect(JSON.stringify(response.text)).toContain('<h2>GROCERIES API!</h2>')
+				})
+			})
+		})
 
-		test("it should return a 200", () => {
-			return request(app)
-			.get("/")
-			.then(response => {
-				expect(response.statusCode).toBe(200);
-			});
-		});
 	})
 
 	describe('when the route is /products', () => {
-		test("it should return all the products", () => {
-			const apple = new Product( { name: 'test-apple' } );
-			const cheese = new Product( { name: 'test-cheese' } );
-			
-			dbHandler.addData(apple, cheese)	
+		
+		describe('when status code is 200', () => {	
+			test("it should return all the products", () => {
+				const apple = new Product( { name: 'test-apple' } );
+				const cheese = new Product( { name: 'test-cheese' } );
+				dbHandler.addData(apple, cheese)	
 
-			return request(app)
-			.get("/products")
-			.then(response => {
-				expect(response.statusCode).toBe(200);
-				expect(JSON.stringify(response.body)).toContain('test-apple')
-				expect(JSON.stringify(response.body)).toContain('test-cheese')
-			});
-			
-		});
+				return request(app)
+				.get("/products")
+				.then(response => {
+
+					expect(response.statusCode).toBe(200);
+					expect(JSON.stringify(response.body)).toContain('test-apple')
+					expect(JSON.stringify(response.body)).toContain('test-cheese')
+				})
+			})
+		})
+
+	})
+
+	describe('when the route is /products/:id', () => {
+
+		describe('when status code is 200', () => {	
+			test("it should return the product", () => {
+				const apple = new Product( { _id: "123123123123123123123123", name: 'test-apple' } );
+				const cheese = new Product( { _id: "321321321321321321321321", name: 'test-cheese' } );
+				dbHandler.addData(apple, cheese)	
+
+				return request(app)
+				.get("/products/321321321321321321321321")
+				.then(response => {
+					expect(response.statusCode).toBe(200);
+					expect(JSON.stringify(response.body)).not.toContain('test-apple')
+					expect(JSON.stringify(response.body)).toContain('test-cheese')
+				})
+			})
+		})
+
 	})
 })
